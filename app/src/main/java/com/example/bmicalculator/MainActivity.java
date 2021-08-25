fa4b03c04aa31b7c8fa8a638798a1b1bd560608e
natalija-margaric <43842722+natalija-margaric@users.noreply.github.com>
2021-08-25 10:57:26 +0200

    java code

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -3,14 +3,58 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+        //fields or class variables
+    private TextView resultText;
+    private RadioButton maleButton;
+    private RadioButton femaleButton;
+    private EditText ageText;
+    private EditText feetText;
+    private EditText inchesText;
+    private EditText weightText;
+    private Button calculateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
 
-        findViewById();
+
+
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "this is working, jeeeeej", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
+
+    private void findViews(){
+        //to make field, shortcut is ctrl+alt+F
+        resultText = findViewById(R.id.text_view_result);
+
+        maleButton = findViewById(R.id.radio_button_male);
+        femaleButton = findViewById(R.id.radio_button_female);
+
+        ageText = findViewById(R.id.edit_text_age);
+        feetText = findViewById(R.id.edit_text_feet);
+        inchesText = findViewById(R.id.edit_text_inches);
+        weightText = findViewById(R.id.edit_text_weight);
+
+        calculateButton = findViewById(R.id.button_calculate);
+    }
+
 }
+
+
